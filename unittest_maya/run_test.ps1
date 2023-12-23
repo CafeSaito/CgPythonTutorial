fc59416e0d36eaefc5c6c4d3d12b101f28b8c44d
CafeSaito <takeshi_saito@cafegroup.info>
2023-12-23 17:44:55 +0900

    update ps1

--- unittest_maya/run_test.ps1
+++ unittest_maya/run_test.ps1
@@ -4,10 +4,10 @@ $script_dir = "$root_dir\scripts"
 $test_dir = "$root_dir\tests"
 
 # テストスクリプトのディレクトリまで移動します。
-cd $test_dir
 
 # 環境変数を設定
 $env:PYTHONPATH = "$script_dir;$env:PYTHONPATH"
 
+cd "$root_dir\tests"
 # mayapy2024を指定してテストを実行
 & "C:\Program Files\Autodesk\Maya2024\bin\mayapy.exe" -m unittest
